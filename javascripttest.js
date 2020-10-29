c847cddab5855f3bc1f64dfb74c79a400191da23
Fares Tellisi <farestellisi11@gmail.com>
2020-10-29 10:40:04 +0000

    Add javascripttest.js

---
+++ javascripttest.js
@@ -0,0 +1,23 @@
+function add7(num){
+    console.log(num + 7)
+};
+
+add7(9);
+
+function multiply(num1, num2){
+    console.log(num1*num2)
+};
+
+multiply(9,7);
+
+function capitalize(string){
+    console.log(string.toUpperCase())
+}
+
+capitalize("Hello World!");
+
+function lastLetter(str){
+    console.log(str.charAt(str.length - 1))
+};
+
+lastLetter("Hello World!");
